f9e7a1e8c69e5fff3b3184104209a3fd3077b20a
mmm25002500 <marryhan25648@gmail.com>
2024-03-02 19:18:15 +0800

    feat: add subscribe section of homepage

---
+++ src/components/HomePage/SubscribeSection.tsx
@@ -0,0 +1,31 @@
+import mail_dark from "@/icons/mail_dark.svg";
+import InputText from "@/components/Input/InputText";
+import { useState } from 'react';
+
+const SubscribeSection = () => {
+  const [email, setEmail] = useState('');
+
+  return (
+    <div className="py-16">
+      <p className="text-2xl leading-9 text-center">
+        訂閱我們電子報
+      </p>
+      <p className="text-sm text-neutral-900 dark:text-neutral-200 text-center py-7">
+        踏上區塊鏈之旅！新手探險家或是渴望區塊鏈領域持續進化的你，<br />這是發現無限可能性的最佳起點！
+      </p>
+      <div className="flex justify-center">
+        <InputText
+          placeholder={"輸入您的 Mail"}
+          btnText="訂閱"
+          text={email}
+          icon={mail_dark}
+          onChange={(e) => setEmail(e.target.value)}
+          onClick={() => console.log('訂閱')}
+          className='w-96'
+        />
+      </div>
+    </div>
+  );
+}
+
+export default SubscribeSection;
--- src/types/Input/Input.ts
+++ src/types/Input/Input.ts
@@ -1,4 +1,9 @@
 export interface InputData {
   placeholder: string;
+  icon: string;
+  btnText: string;
+  text: string;
+  onClick: () => void;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   className?: string;
 }
