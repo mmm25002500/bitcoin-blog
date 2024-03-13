b49696eb412b05ba7fdbc22d108e81d226c7e5c5
mmm25002500 <marryhan25648@gmail.com>
2024-03-14 05:48:09 +0800

    feat: add Privacy Policy

---
+++ src/pages/PrivacyPolicy.tsx
@@ -0,0 +1,40 @@
+import HorizontalLine from '@/components/HorizontalLine';
+import ArticalLayout from '@/components/Layout/Artical/AriticalLayout';
+import ArticalHeader from '@/components/Layout/Artical/ArticalHeader';
+import Navbar from '@/components/Layout/Navbar';
+import MD from '@/components/MD'
+import ContactSection from '@/components/Page/ContactSection';
+import SubscribeSection from '@/components/Page/SubscribeSection';
+import IconLight from '@/icons/illustation/Privacy Policy.svg';
+
+const PrivacyPolicyPage = () => {
+  const markdown = " \
+  我們非常重視您的隱私。我們的網站不收集任何有關您的個人信息，如姓名、地址、電子郵件地址或電話號碼等。我們不會通過 cookie 或其他技術跟踪您的網絡活動。 \n\n \
+  儘管我們不收集任何個人信息，但我們可能會通過網站日誌收集一些匿名數據，例如您的瀏覽器類型和操作系統等信息。這些信息只用於統計和分析我們網站的流量，以及提高網站的性能和用戶體驗。 \n\n \
+  請注意，我們的網站可能包含第三方鏈接，這些鏈接指向其他網站，我們無法控制這些網站的隱私政策。因此，我們建議您在訪問這些網站之前查看其隱私政策。\n\n \
+  如果您有任何關於我們的隱私政策的問題，請聯繫我們。 \n\n \
+  "
+
+  return (
+    <>
+      <Navbar />
+      <ArticalHeader
+        title="Privacy Policy"
+        subtitle="隱私政策聲明"
+        icon={IconLight}
+      />
+      <div className="mx-auto sm:px-28">
+        <ArticalLayout className='pt-10 px-5 sm:px-0'>
+          <MD>{markdown}</MD>
+        </ArticalLayout>
+        <HorizontalLine className='sm:hidden' />
+        <ContactSection className="py-16" />
+        <HorizontalLine />
+        <SubscribeSection className="py-16" />
+        <HorizontalLine />
+      </div>
+    </>
+  )
+}
+
+export default PrivacyPolicyPage;
