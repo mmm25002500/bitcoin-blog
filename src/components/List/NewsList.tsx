b9770e22644d080dc82f3eb506f6ef52d0f5fd6a
mmm25002500 <marryhan25648@gmail.com>
2024-07-04 01:16:46 +0800

    feat: add btn click show perpage

--- src/components/List/NewsList.tsx
+++ src/components/List/NewsList.tsx
@@ -4,22 +4,28 @@ import Post from "./Post";
 import Button from '../Button/Button';
 import { useRouter } from "next/router";
 import { PostProps } from '@/types/List/PostData';
+import postListConfig from '@/config/SiteConfig.json';
 
 const PostList = (props: PostListData) => {
-  const [showAll, setShowAll] = useState(false);
+  const [postsToShow, setPostsToShow] = useState(3);
+  const postsPerPage = postListConfig.HomePageNewsListPerpage;
 
   const router = useRouter();
 
   // 以日期排序
   const sortedData = props.data.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
 
-  // 先六個
-  const postsToShow = showAll ? sortedData : sortedData.slice(0, 6);
+  // 顯示的帖子數量
+  const displayedPosts = sortedData.slice(0, postsToShow);
+
+  // 處理按下按鈕時顯示更多文章
+  const handleShowMore = () => {
+    setPostsToShow((prevPostsToShow) => Math.min(prevPostsToShow + postsPerPage, sortedData.length));
+  };
 
   return (
     <>
-
-      {postsToShow.map((post: PostProps, index) => (
+      {displayedPosts.map((post: PostProps, index) => (
         <Post
           key={index}
           onClick={() => router.push(`/News/${post.authorData?.id}/${post.id}`)}
@@ -35,11 +41,11 @@ const PostList = (props: PostListData) => {
         />
       ))}
 
-      {/* 查看更多 */}
-      {!showAll && (
+      {/* 查看更多按鈕 */}
+      {postsToShow < sortedData.length && (
         <div className='flex justify-center py-10'>
           <Button
-            onClick={() => setShowAll(true)}
+            onClick={handleShowMore}
             type="large"
             className="dark:!border-neutral-white dark:!bg-neutral-white dark:!text-primary-black-300 flex items-center gap-2 w-44 justify-center bg-neutral-black text-neutral-white">
             More
