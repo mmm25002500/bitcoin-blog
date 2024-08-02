bec56725a15c507174f4e31fc1eaa5d218d18df4
mmm25002500 <marryhan25648@gmail.com>
2024-08-02 20:27:21 +0800

    fix: theme btc items center

--- src/components/Layout/Themes.tsx
+++ src/components/Layout/Themes.tsx
@@ -18,21 +18,25 @@ const Themes = () => {
         theme === 'dark' ? (
           <div
             className="rounded-full w-8 h-8 border-neutral-700 dark:bg-primary-black-300 relative dark:ml-[3.6rem] pointer-events-none transition-all duration-300 ease-out">
-            <Image
-              src={ light }
-              alt="Images Not Found"
-              className="w-[50%] h-[50%] mr-auto ml-auto inline-block align-middle">
-            </Image>
+            <div className="flex items-center h-full">
+              <Image
+                src={light}
+                alt="Images Not Found"
+                className="w-[50%] h-[50%] mr-auto ml-auto inline-block align-middle">
+              </Image>
+            </div>
           </div>
         ) : (
           // 如果是 light mode
           <div
             className="rounded-full border-neutral-200 border-[1px] w-8 h-8 bg-neutral-white relative dark:ml-0 pointer-events-none transition-all duration-300 ease-out">
-            <Image
-              src={ Moon }
-              alt="Images Not Found"
-              className="w-[50%] h-[50%] mr-auto ml-auto inline-block align-middle">
-            </Image>
+            <div className="flex items-center h-full">
+              <Image
+                src={Moon}
+                alt="Images Not Found"
+                className="w-[50%] h-[50%] mr-auto ml-auto inline-block align-middle">
+              </Image>
+            </div>
           </div>
         )
       }
