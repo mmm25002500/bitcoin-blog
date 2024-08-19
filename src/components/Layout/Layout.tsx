e7cf12d8471750c5e689aa5aa16d0c12e30be670
mmm25002500 <marryhan25648@gmail.com>
2024-08-20 04:36:05 +0800

    fix: change theme color

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,14 +2,23 @@ import Head from "next/head"
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import { LayoutData } from "@/types/Layout/Layout";
+import { useTheme } from 'next-themes';
 
 const Layout = ({ children }: LayoutData) => {
+  const { theme } = useTheme();
+
   return (
     <>
       {/* HEAD */}
       <Head>
         <title>比特幣中文標題</title>
         <meta name="description" content="比特幣中文" />
+        {
+          theme === 'dark' ?
+            <meta name="theme-color" content="#1c1c1c" />
+            :
+            <meta name="theme-color" content="#ffffff" />
+        }
       </Head>
 
       {/* Body */}
