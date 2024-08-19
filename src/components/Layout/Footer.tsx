6e508ed3e255de37b406d18bca5d45f21a593a42
mmm25002500 <marryhan25648@gmail.com>
2024-08-19 08:21:44 +0800

    feat: footer width 100%

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -10,66 +10,66 @@ const Footer = () => {
   return (
     <>
       <hr className='border-[#E7E6F2] dark:border-1' />
-      {/* figma: px-10 pt-10 */}
-      <div className="h-[15.75rem] dark:bg-primary-black-300 px-5 pt-5 pb-[3.75rem] gap-8">
+        {/* figma: px-10 pt-10 */}
+        <div className="dark:bg-primary-black-300 px-5 pt-5 pb-[3.75rem] gap-8">
 
-        {/* Link */}
-        {/* figma: gap-6 */}
-        <div className='flex gap-6 justify-center pt-20'>
-          <Link
-            href='/Disclaimer'
-            prefetch={false}
-            className=""
-          >
-            <p>聲明</p>
-          </Link>
-          <Link
-            href='/PrivacyPolicy'
-            prefetch={false}
-            className=""
-          >
-            <p>隱私權</p>
-          </Link>
-          <Link
-            href='/About'
-            prefetch={false}
-            className=""
-          >
-            <p>關於</p>
-          </Link>
-          <Link
-            href='/'
-            prefetch={false}
-            className=""
-          >
-            <p>支持</p>
-          </Link>
+          {/* Link */}
+          {/* figma: gap-6 */}
+          <div className='flex gap-6 justify-center pt-20'>
+            <Link
+              href='/Disclaimer'
+              prefetch={false}
+              className=""
+            >
+              <p>聲明</p>
+            </Link>
+            <Link
+              href='/PrivacyPolicy'
+              prefetch={false}
+              className=""
+            >
+              <p>隱私權</p>
+            </Link>
+            <Link
+              href='/About'
+              prefetch={false}
+              className=""
+            >
+              <p>關於</p>
+            </Link>
+            <Link
+              href='/'
+              prefetch={false}
+              className=""
+            >
+              <p>支持</p>
+            </Link>
+          </div>
+
+          {/* Icon */}
+          <div className='ml-5'>
+            <Icon
+              icon_light={btc_icon_dark}
+              icon_dark={btc_icon_light}
+              className='mr-auto ml-auto mt-24'
+            />
+          </div>
+
+          {/* Copyright */}
+          <div className='flex flex-col gap-14 justify-center pt-10 pb-28'>
+            <p className='text-neutral-600 dark:text-neutral-300 text-center'>
+              © Bitcoin.zh <br />
+            </p>
+            <p className='ml-5 text-neutral-600 dark:text-neutral-300 text-center'>
+              2140
+            </p>
+          </div>
+
         </div>
-
-        {/* Icon */}
-        <div className='ml-5'>
-          <Icon
-            icon_light={btc_icon_dark}
-            icon_dark={btc_icon_light}
-            className='mr-auto ml-auto mt-24'
-          />
-        </div>
-
-        {/* Copyright */}
-        <div className='flex flex-col gap-14 justify-center pt-10 pb-28'>
-          <p className='text-neutral-600 dark:text-neutral-300 text-center'>
-            © Bitcoin.zh <br />
-          </p>
-          <p className='ml-5 text-neutral-600 dark:text-neutral-300 text-center'>
-            2140
-          </p>
-        </div>
-
         <Image
           src={IMG}
           alt={''}
         />
-      </div>
     </>
   )
 }
