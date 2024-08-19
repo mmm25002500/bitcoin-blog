0fcfc4fda42697284e4fa013c23e39bf438d1b32
mmm25002500 <marryhan25648@gmail.com>
2024-08-20 04:18:04 +0800

    fix: change theme color

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,8 +7,8 @@ const Document = () => {
   return (
     <Html lang="en">
       <Head>
-        <meta name="theme-color" content="#ffffff" media="(prefers-color-scheme: light)" />
-        <meta name="theme-color" content="#1c1c1c" media="(prefers-color-scheme: dark)" />
+        <meta name="theme-color" content="#1c1c1c" media="(prefers-color-scheme: light)" />
+        <meta name="theme-color" content="#ffffff" media="(prefers-color-scheme: dark)" />
       </Head>
       <body className='text-black bg-white dark:bg-primary-black-300 dark:text-white /*樣式變更動畫 transition-colors duration-100*/'>
         <Main />
