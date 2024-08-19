7dd2e5ec092a5aa8c57b6867d14a61e74fbfa3ec
mmm25002500 <marryhan25648@gmail.com>
2024-08-19 10:11:19 +0800

    fix: footer image white line

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,7 @@ const Document = () => {
   return (
     <Html lang="en">
       <Head>
-        <meta name="theme-color" content={theme === 'dark' ? '#ffffff': '#1c1c1c'} />
+        <meta name="theme-color" content={theme === 'dark' ? '#1c1c1c': '#ffffff'} />
       </Head>
       <body className='text-black bg-white dark:bg-primary-black-300 dark:text-white /*樣式變更動畫 transition-colors duration-100*/'>
         <Main />
