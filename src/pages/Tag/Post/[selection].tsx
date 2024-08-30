c3596867d9e658df2c109a0e17bffa8ecb017797
mmm25002500 <marryhan25648@gmail.com>
2024-08-30 18:53:36 +0800

    fix: tag post page arrow index

--- src/pages/Tag/Post/[selection].tsx
+++ src/pages/Tag/Post/[selection].tsx
@@ -113,7 +113,7 @@ const All = ({ initialPosts, initialSelection, seo, tags, SiteConfig }: { initia
             </div>
 
             {/* 左右箭頭 */}
-            <div className="swiper-button-prev absolute left-0 top-1/2 transform -translate-y-1/2 z-10">
+            <div className="swiper-button-prev absolute left-0 top-1/2 transform -translate-y-1/2 z-0">
               <Image
                 src={left}
                 alt="Icon Dark"
@@ -122,7 +122,7 @@ const All = ({ initialPosts, initialSelection, seo, tags, SiteConfig }: { initia
                 className="rounded-full w-5 h-5 dark:invert"
               />
             </div>
-            <div className="swiper-button-next absolute right-0 top-1/2 transform -translate-y-1/2 z-10">
+            <div className="swiper-button-next absolute right-0 top-1/2 transform -translate-y-1/2 z-0">
               <Image
                 src={right}
                 alt="Icon Dark"
