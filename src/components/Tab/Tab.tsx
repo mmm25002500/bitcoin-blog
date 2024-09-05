6b878b48da17477b8b76d8bd2a10a9ae6de895d7
mmm25002500 <marryhan25648@gmail.com>
2024-09-05 21:05:55 +0800

    feat: add Tag swiper page

--- src/components/Tab/Tab.tsx
+++ src/components/Tab/Tab.tsx
@@ -15,10 +15,10 @@ const Tab = (props: TabDataProps) => {
             name="tab"
             value={tab.name}
             checked={props.selectedTab === tab.name}
-            onChange={() => handleTabChange(tab.name, tab.link)}
+            onChange={() => handleTabChange(tab.link, tab.link)}
             className="hidden"
           />
-          <p className={`font-medium text-base leading-6 inline-block px-3 pb-2 ${props.selectedTab === tab.name ? 'border-b-2 border-neutral-black dark:border-white text-neutral-black dark:text-neutral-white' : 'text-neutral-800 dark:text-neutral-600'}`}>
+          <p className={`font-medium text-base leading-6 inline-block px-3 pb-2 ${props.selectedTab === tab.link ? 'border-b-2 border-neutral-black dark:border-white text-neutral-black dark:text-neutral-white' : 'text-neutral-800 dark:text-neutral-600'}`}>
             {tab.name}
           </p>
         </label>
