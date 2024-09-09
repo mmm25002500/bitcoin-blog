9136bcd254dae5aab14bc64223422da76a7edad5
mmm25002500 <marryhan25648@gmail.com>
2024-09-09 20:45:59 +0800

    feat: moreBTC title smaller

--- src/components/Drawer/MoreInfoDrawer.tsx
+++ src/components/Drawer/MoreInfoDrawer.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Drawer } from "@material-tailwind/react";
+import { Card, Drawer } from "@material-tailwind/react";
 import { SidebarProps } from "@/types/Drawer/MoreInfoDrawer";
 import { useRouter } from 'next/navigation';
 import Image from "next/image";
@@ -42,54 +42,63 @@ const MoreInfoDrawer = (props: SidebarProps) => {
           onClick: closeDrawer
         }}
       >
-        <p className="my-5 text-black dark:text-white text-center font-medium text-sm leading-[22px]">
-          類別
-        </p>
-        {/* 類別 */}
+        <Card
+          color="transparent"
+          shadow={false}
+          className="h-full w-full overflow-y-auto"
+          nonce={undefined}
+          onResize={undefined}
+          onResizeCapture={undefined}
+        >
+          <p className="my-5 text-black dark:text-white text-center font-medium text-sm leading-[22px]">
+            類別
+          </p>
+          {/* 類別 */}
 
-        <div className={`m-5 ${props.className}`}>
-          {props.data.map((category: categoryData, index: number) => (
-            <div key={index} className="border-b-[1px] border-[#E7E6F2] dark:border-neutral-800 p-2">
-              <p className="text-black dark:text-white">{ category.label }</p>
-              {/* 類別 */}
-              <button
-                className="text-neutral-black dark:text-neutral-white w-full text-left font-medium text-sm leading-[22px] px-3 py-2 cursor-pointer flex justify-between items-center"
-                onClick={() => handleToggle(index)}
-              >
-                {category.title}
+          <div className={`m-5 ${props.className}`}>
+            {props.data.map((category: categoryData, index: number) => (
+              <div key={index} className="border-b-[1px] border-[#E7E6F2] dark:border-neutral-800 p-2">
+                <p className="text-black dark:text-white text-sm">{category.label}</p>
+                {/* 類別 */}
+                <button
+                  className="text-neutral-black dark:text-neutral-white w-full text-left font-medium text-sm leading-[22px] px-3 py-2 cursor-pointer flex justify-between items-center"
+                  onClick={() => handleToggle(index)}
+                >
+                  {category.title}
 
-                {/* 箭頭 */}
-                <Image
-                  src={DownIcon}
-                  alt="Icon Dark"
-                  className={`transition-transform duration-200 dark:invert ${expandedIndex === index ? 'transform rotate-180' : ''}`}
-                ></Image>
-              </button>
+                  {/* 箭頭 */}
+                  <Image
+                    src={DownIcon}
+                    alt="Icon Dark"
+                    className={`transition-transform duration-200 dark:invert ${expandedIndex === index ? 'transform rotate-180' : ''}`}
+                  ></Image>
+                </button>
 
-              {/* 文章 */}
-              {expandedIndex === index && (
-                <ul className="pl-3">
-                  {category.post.map((post, postIndex) => (
-                    <li key={postIndex} className={`py-2 ${props.path === `${post.filename}` ? 'bg-neutral-100 text-neutral-black dark:bg-neutral-900 dark:text-neutral-white rounded-[4px]' : 'text-neutral-800 dark:text-neutral-300'}`}>
-                      <button
-                        onClick={
-                          () => {
-                            props.onChange(post.filename);
-                            router.push(`/moreBTC/${post.filename}`);
-                            closeDrawer();
-                          }}
-                      >
-                        <span className={`pl-4 font-normal text-sm leading-6 break-all block text-left`}>
-                          {post.title}
-                        </span>
-                      </button>
-                    </li>
-                  ))}
-                </ul>
-              )}
-            </div>
-          ))}
-        </div>
+                {/* 文章 */}
+                {expandedIndex === index && (
+                  <ul className="pl-3">
+                    {category.post.map((post, postIndex) => (
+                      <li key={postIndex} className={`py-2 ${props.path === `${post.filename}` ? 'bg-neutral-100 text-neutral-black dark:bg-neutral-900 dark:text-neutral-white rounded-[4px]' : 'text-neutral-800 dark:text-neutral-300'}`}>
+                        <button
+                          onClick={
+                            () => {
+                              props.onChange(post.filename);
+                              router.push(`/moreBTC/${post.filename}`);
+                              closeDrawer();
+                            }}
+                        >
+                          <span className={`pl-4 font-normal text-sm leading-6 break-all block text-left`}>
+                            {post.title}
+                          </span>
+                        </button>
+                      </li>
+                    ))}
+                  </ul>
+                )}
+              </div>
+            ))}
+          </div>
+        </Card>
       </Drawer>
     </>
   );
