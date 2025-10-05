035c3790427b7fa418d5e6d450dc5fbd856fa63b
mmm25002500 <marryhan25648@gmail.com>
2025-10-06 03:31:50 +0800

    feat: add supabase

--- next.config.js
+++ next.config.js
@@ -1,62 +1,71 @@
 const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/Tag/News',
-        destination: `/Tag/News/all`,
-        permanent: false,
-      },
-      {
-        source: '/Tag/Post',
-        destination: `/Tag/Post/all`,
-        permanent: false,
-      },
-      {
-        source: '/Tag',
-        destination: `/Tag/all`,
-        permanent: false,
-      },
-      {
-        source: '/Search',
-        destination: `/Search/Posters/標籤1`,
-        permanent: false,
-      },
-      {
-        source: '/Search/:path',
-        destination: `/Search/Posters/:path`,
-        permanent: false,
-      },
-      {
-        source: '/Search/Posters',
-        destination: `/Search/Posters/標籤1`,
-        permanent: false,
-      },
-      {
-        source: '/Search/News',
-        destination: `/Search/News/文字1`,
-        permanent: false,
-      },
-      {
-        source: '/Search/Creators',
-        destination: `/Search/Crators/%20`,
-        permanent: false,
-      },
-    ];
-  },
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "tjdnszvgfvxoxdangdhu.supabase.co",
+				pathname: "/storage/v1/object/public/**",
+			},
+		],
+	},
+	async redirects() {
+		return [
+			{
+				source: "/Tag/News",
+				destination: "/Tag/News/all",
+				permanent: false,
+			},
+			{
+				source: "/Tag/Post",
+				destination: "/Tag/Post/all",
+				permanent: false,
+			},
+			{
+				source: "/Tag",
+				destination: "/Tag/all",
+				permanent: false,
+			},
+			{
+				source: "/Search",
+				destination: "/Search/Posters/標籤1",
+				permanent: false,
+			},
+			{
+				source: "/Search/:path",
+				destination: "/Search/Posters/:path",
+				permanent: false,
+			},
+			{
+				source: "/Search/Posters",
+				destination: "/Search/Posters/標籤1",
+				permanent: false,
+			},
+			{
+				source: "/Search/News",
+				destination: "/Search/News/文字1",
+				permanent: false,
+			},
+			{
+				source: "/Search/Creators",
+				destination: "/Search/Crators/%20",
+				permanent: false,
+			},
+		];
+	},
 
-  webpack: (config, { isServer }) => {
-    // 解決 PostCSS 錯誤
-    config.resolve.alias['postcss'] = require.resolve('postcss');
+	webpack: (config, { isServer }) => {
+		// 解決 PostCSS 錯誤
+		config.resolve.alias.postcss = require.resolve("postcss");
 
-    // 修復使用 fs 模塊的依賴
-    if (!isServer) {
-      config.resolve.fallback = { fs: false };
-    }
+		// 修復使用 fs 模塊的依賴
+		if (!isServer) {
+			config.resolve.fallback = { fs: false };
+		}
 
-    return config;
-  },
+		return config;
+	},
 
-  reactStrictMode: true,
+	reactStrictMode: true,
 };
 
 module.exports = nextConfig;
