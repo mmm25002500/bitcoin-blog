8feca26fbaba22bd3fe5159bed7303b87db0038e
mmm25002500 <marryhan25648@gmail.com>
2025-11-23 22:18:38 +0800

    fix: add purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -2,5 +2,43 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    ...(process.env.NODE_ENV === 'production'
+      ? {
+          '@fullhuman/postcss-purgecss': {
+            content: [
+              './src/pages/**/*.{js,ts,jsx,tsx}',
+              './src/components/**/*.{js,ts,jsx,tsx}',
+            ],
+            defaultExtractor: (content) =>
+              content.match(/[\w-/:]+(?<!:)/g) || [],
+            safelist: {
+              standard: [
+                /^swiper/,
+                /^dark/,
+                /^drawer/,
+                /^card/,
+                /^bg-/,
+                /^text-/,
+                /^border-/,
+                /^prose/,
+                /^mt-/,
+                /^mb-/,
+                /^ml-/,
+                /^mr-/,
+                /^mx-/,
+                /^my-/,
+                /^p-/,
+                /^pt-/,
+                /^pb-/,
+                /^pl-/,
+                /^pr-/,
+                /^px-/,
+                /^py-/,
+              ],
+              deep: [/drawer/, /card/, /swiper/],
+            },
+          },
+        }
+      : {}),
   },
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -17,7 +17,16 @@ const Document = () => {
 
   return (
     <Html lang="en" className="">
-      <Head />
+      <Head>
+        {/* Preload LCP image */}
+        <link
+          rel="preload"
+          as="image"
+          href="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FS__4964469.221cc2d9.jpg&w=1920&q=75"
+          // @ts-ignore
+          fetchpriority="high"
+        />
+      </Head>
       <body className={"text-black dark:text-white m-0 p-0"}>
         <Main />
         <NextScript />
