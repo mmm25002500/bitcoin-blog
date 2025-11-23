087721ae2d9a26b316ddd7926d022e06243ab8e1
mmm25002500 <marryhan25648@gmail.com>
2025-11-23 22:24:05 +0800

    fix: postcss error

--- postcss.config.js
+++ postcss.config.js
@@ -1,46 +1,6 @@
-const purgecss = require('@fullhuman/postcss-purgecss');
-
 module.exports = {
-  plugins: [
-    'tailwindcss',
-    'autoprefixer',
-    ...(process.env.NODE_ENV === 'production'
-      ? [
-          purgecss({
-            content: [
-              './src/pages/**/*.{js,ts,jsx,tsx}',
-              './src/components/**/*.{js,ts,jsx,tsx}',
-            ],
-            defaultExtractor: (content) =>
-              content.match(/[\w-/:]+(?<!:)/g) || [],
-            safelist: {
-              standard: [
-                /^swiper/,
-                /^dark/,
-                /^drawer/,
-                /^card/,
-                /^bg-/,
-                /^text-/,
-                /^border-/,
-                /^prose/,
-                /^mt-/,
-                /^mb-/,
-                /^ml-/,
-                /^mr-/,
-                /^mx-/,
-                /^my-/,
-                /^p-/,
-                /^pt-/,
-                /^pb-/,
-                /^pl-/,
-                /^pr-/,
-                /^px-/,
-                /^py-/,
-              ],
-              deep: [/drawer/, /card/, /swiper/],
-            },
-          }),
-        ]
-      : []),
-  ],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 }
